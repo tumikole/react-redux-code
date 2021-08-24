c8f424f77a994937d341ed14d6e6bf9765236efb
tumikole <ramollosamuel@2gmail.com>
2021-08-24 15:54:17 +0200

    adding redux to the app

---
+++ react-redux-code/src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      name: '',
+      surname: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({...this.state,[event.target.name]: event.target.value});
+  }
+
+  onSubmit(event){
+    const newList = []
+      event.preventDefault();
+      this.setState({
+        
+        ...this.state,list:[...this.state.list, {name:this.state.name,surname:this.state.surname}]
+      })
+      newList.push({...this.state,list:[...this.state.list, {name:this.state.name,surname:this.state.surname}]})
+  }
+  
+  
+  
+  render() {
+    
+    
+    return (
+<div>
+      <form>
+        
+          Name:
+          <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+          Surname
+          <input name="surname" type="text" value={this.state.surname} onChange={this.handleChange} />
+          <button type="submit" value="submit" onClick={this.onSubmit}/>
+      </form>
+
+{this.state.list.map((list, index) =>{
+  return(
+
+
+    <table>
+      <tr>
+<th>Name</th>
+<th>Surname</th>
+
+
+      </tr>
+    
+      <tr key={index}>
+         <td>
+           {list.name}
+
+         </td>
+         <td>
+           {list.surname}
+           
+         </td>
+      </tr>
+      </table>
+  )
+})}
+
+      
+      </div>
+     )};
+     };
+export default App;
